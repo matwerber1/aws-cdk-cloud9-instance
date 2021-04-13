dcfda64bb6cd9cc627a8b6de474a01dc728c107d
Mathew Werber <werberm@amazon.com>
2021-04-13 12:57:23 -0700

    configure account and region

--- bin/cdk-cloud9.ts
+++ bin/cdk-cloud9.ts
@@ -5,6 +5,7 @@ import { CdkCloud9Stack } from '../lib/cdk-cloud9-stack';
 
 const app = new cdk.App();
 new CdkCloud9Stack(app, 'CdkCloud9Stack', {
+  
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
@@ -18,4 +19,13 @@ new CdkCloud9Stack(app, 'CdkCloud9Stack', {
   // env: { account: '123456789012', region: 'us-east-1' },
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+
+  // You don't necessarilly always need to provide account/region. In many cases, this can be
+  // inferred by CDK based on the AWS credentials you are using. However, since I am importing a pre-existing
+  // VPC in lib/cdk-cloud9-stack.ts, CDK does not know which account/region that VPC belongs to and I need to
+  // explicitly define it here:
+  env: {
+    account: '999999999999',
+    region: 'us-west-2'
+  }
 });
